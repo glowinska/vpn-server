e48ba4c76184b4dca7f24e2daff81af630f112f0
glowinska <anna.glowinska98@gmail.com>
2020-11-02 15:25:02 +0100

    Script extended by generating a private VPN Server key and using it to sign the VPN Server certificate

--- create_certificate.sh
+++ create_certificate.sh
@@ -10,4 +10,8 @@ ipsec pki --self --ca --lifetime 3650 --in private/strongswanKey.der --type rsa 
 
 ipsec pki --gen --type rsa --size 4096 --outform der > private/vpnHostKey.der
 
+chmod 600 private/vpnHostKey.der
 
+ipsec pki --pub --in private/vpnHostKey.der --type rsa | ipsec pki --issue --lifetime 3650 --cacert cacerts/strongswanCert.der -cakey private/strongswanKey.der --dn "C=PL, O=ServerName, CN=ServerIP.pl" --san @ServerIP.pl -flag serverAuth --flag ikeIntermediate --outform der > certs/vpnHostCert.der
+
+openssl x509 -inform DER -in cacerts/strongswanCert.der -out cacerts/strongswanCert.pem -outform PEM
